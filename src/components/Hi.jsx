fb2e6cf0e298cac1219da71e736b0cb0efc902e7
unknown <aswinsajan91@gmail.com>
2023-09-25 22:30:30 +0530

    splineFix

--- src/components/Hi.jsx
+++ src/components/Hi.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const Hi = () => {
   return (
-    <div className="text-white absolute top-[20vh] p-5">
+    <div className="text-white absolute top-[20vh] left-[10vw] p-5">
       <h1 className="text-3xl">Hi </h1>
       <h1 className="text-5xl">I,m Aswin</h1>
+      <h1 className="text-5xl">Frontend Developer</h1>
     </div>
   );
 };
