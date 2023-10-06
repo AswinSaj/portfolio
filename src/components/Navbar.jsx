5e72df115bcf94a417de7ecbaa8d4f6080983d96
unknown <aswinsajan91@gmail.com>
2023-10-06 09:10:02 +0530

    minor adjustments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,12 @@ const Navbar = () => {
     <div className="md:py-5 md:px-10 py-5  bg-[#040919] sticky z-10 top-0 bg-opacity-90 px-0">
       <div className="flex justify-between">
         <div>
-          <h1 className="text-white text-2xl md:text-3xl font-bold pl-7 hover:animate-wiggle">
+          <h1 className="text-white text-2xl md:text-3xl font-bold md:pl-7 pl-1 hover:animate-wiggle">
             Aswin.dev
           </h1>
         </div>
 
-        <ul className="md:flex md:gap-7 md:items-center md:w-auto  flex items-center gap-3 text-sm md:text-base  text-white">
+        <ul className="md:flex md:gap-7 md:items-center md:w-auto  flex pl-1 items-center gap-3 text-sm md:text-base  text-white">
           <Link to="#about">
             <li className="hover:text-[#5EA5C1]">About</li>
           </Link>
--- src/components/Spline.jsx
+++ src/components/Spline.jsx
@@ -3,7 +3,7 @@ const Spline = lazy(() => import("@splinetool/react-spline"));
 
 export default function Scene() {
   return (
-    <div className="relative h-screen w-screen md:w-[99vw]">
+    <div className="relative h-screen w-screen ">
       <Suspense fallback={<div>Loading...</div>}>
         <Spline scene="https://prod.spline.design/OuTgKD4-C0b6ZDln/scene.splinecode" />
       </Suspense>
